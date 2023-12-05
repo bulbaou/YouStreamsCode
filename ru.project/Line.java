177012f9aa3930520da4f1faee393287e8749e42
bulbaou <ougrobko-1@yandex.ru>
2023-12-05 19:13:48 +0400

    task 1.4

--- ru.project/Line.java
+++ ru.project/Line.java
@@ -2,6 +2,15 @@ public class Line {
     Point start;
     Point end;
 
+    Line (Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+    Line (int x1, int y1, int x2, int y2) {
+        start = new Point(x1, y1);
+        end = new Point(x2, y2);
+    }
+
     @Override
     public String toString() {
         return "Линия от " + start + " до " + end;
--- ru.project/Point.java
+++ ru.project/Point.java
@@ -1,5 +1,12 @@
 public class Point {
     int x,y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
     public String toString() {
         return "{" + x + ";" + y + "}";
     }
